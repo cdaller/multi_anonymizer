bd87bc9e58cd6c787edb7667ee447c8055ca66ff
Christof Dallermassl <christof.dallermassl@43bits.com>
2023-09-06 13:33:28 +0200

    add json in test database

--- testfiles/create_sqlite.py
+++ testfiles/create_sqlite.py
@@ -3,6 +3,7 @@
 import sqlite3
 from faker import Faker
 import sys
+import json
 
 # Define the SQLite database connection
 if len(sys.argv) > 1:
@@ -24,7 +25,8 @@ CREATE TABLE IF NOT EXISTS people (
     first_name TEXT,
     last_name TEXT,
     age INTEGER,
-    email TEXT
+    email TEXT,
+    json TEXT,
 );
 '''
 cursor.execute(create_table_query)
@@ -36,9 +38,18 @@ for _ in range(10):  # Adjust the number of entries you want to add
     last_name = fake.last_name()
     age = fake.random_int(min=18, max=99)
     email = fake.word() + '@example.com'
+    json_obj = {
+        "person": {
+            "lastname": last_name,
+            "firstname": first_name,
+            "age": age,
+            "email": email,
+        }
+    }
+    json_string_pretty = json.dumps(json_obj, indent=4)
     
-    insert_query = "INSERT INTO people (first_name, last_name, age, email) VALUES (?, ?, ?, ?)"
-    cursor.execute(insert_query, (first_name, last_name, age, email))
+    insert_query = "INSERT INTO people (first_name, last_name, age, email, json) VALUES (?, ?, ?, ?, ?)"
+    cursor.execute(insert_query, (first_name, last_name, age, email, json_string_pretty))
 
 # insert a null values as a valid test case
 insert_query = "INSERT INTO people (first_name, last_name, age) VALUES (?, ?, ?)"
